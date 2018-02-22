8979be48a89cfa18e2697c326ebad9a66cedd9e1
ayushspark11 <ayushspark11@gmail.com>
2018-02-21 21:16:51 -0500

    Added some database communication
    Added some db communication functions to pull information from the database. only enough to pull for word pages. will need to add functionality for pulling information for user pages and functions for modifying database contents

--- Connection.js
+++ Connection.js
@@ -3,24 +3,62 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "sql123"
+    //password will need to be replaced with local root password
+    //password: "sql123",
+    password: "Overdrive#11",
+    database: "expressionary_data"
 });
 
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected")
-
-    var sql = "USE expressionary_data"
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-      //  console.log("Result: " + result);
-    });
-
-    sql = "select word from wordpage where wp_id=4 group by wp_id"
-
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log(result);
-    });
 });
 
+//if one field does not exist whole query returns null
+function getwpFromWord(word) {
+    /*var request = "SELECT posts.date, posts.definition, posts.points, user.username, wordpage.totalPoints FROM wordpage, posts, users WHERE wordpage.word = " + word + "," +
+        "posts.wordpage_wp_id = wordpage.wp_id, users.user_id = posts.users_user_id";
+    var wordPageData = [];
+    var wordId = 0;
+    con.query(request, function(err, result) {
+        if(err) return "notFound";
+        for(var i in result) {
+            wordPageData.push(i);
+        }
+        console.log(result);
+    });*/
+    //var wordPageData = [];
+    var requestWord = "SELECT wp_id, totalPoints FROM wordpage WHERE word = '" + word + "'";
+    //console.log(requestWord);
+    con.query(requestWord, function(err, result) {
+        if(err) return "notFound";
+        //for(var i in result) {
+        //    wordPageData.push(i);
+        //}
+        //format: [RowDataPacket { wp_id: int, totalPoints: int}]
+        return(result);
+    });
+}
+
+function getPostsFromWordId(wp_id) {
+    var request = "SELECT posts.date, posts.points, posts.definition, users.username FROM posts, users WHERE posts. wordPage_wp_id = " + wp_id + "&& posts.users_user_id = users.user_id";
+    con.query(request, function(err, results) {
+        if(err) return "notFound";
+        else {
+            //format: [RowDataPacket { date: datetime variable, points: 0, definition: text, username: text}]
+            return(results);
+        }
+    });
+}
+
+function findUserByEmail(email) {
+
+}
+
+function findUserByUsername(username) {
+
+}
+
+function getPassword(username) {
+
+}
